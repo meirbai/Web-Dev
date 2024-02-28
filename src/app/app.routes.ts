64211bb9e2e117861d4c94e7dd143e63bdce7775
meirbai <123232231+meirbai@users.noreply.github.com>
2024-02-28 20:52:24 +0600

    Delete src directory

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import {ProductListComponent} from "./product-list/product-list.component";
-import {ProductItemsComponent} from "./product-items/product-items.component";
-
-export const routes: Routes = [
-    {path:'telefony', component: ProductListComponent},
-    {path:':type', component:ProductItemsComponent}
-];
